2247c53c54080681cfba4407190e73f7a00c944d
Reüel van der Steege <reuel@pronamic.nl>
2023-02-23 10:03:50 +0100

    Coding standards.

--- tests/wp-env-mail.php
+++ tests/wp-env-mail.php
@@ -1,6 +1,11 @@
 <?php
-/*
+/**
  * Plugin Name: wp-env mail
+ *
+ * @author    Pronamic <info@pronamic.eu>
+ * @copyright 2005-2023 Pronamic
+ * @license   GPL-3.0-or-later
+ * @package   Pronamic\WordPress\Pay\Extensions\ContactForm7
  */
 
 /**
@@ -8,6 +13,9 @@
  * 
  * @link https://github.com/WordPress/wordpress-develop/blob/2f8f1fc795789530db31bdf020e1d96acf02a760/src/wp-includes/pluggable.php#L194-L214
  */
-add_filter( 'pre_wp_mail', function( $short_circuit ) {
-	return true;
-} );
+add_filter(
+	'pre_wp_mail',
+	function( $short_circuit ) {
+		return true;
+	} 
+);
